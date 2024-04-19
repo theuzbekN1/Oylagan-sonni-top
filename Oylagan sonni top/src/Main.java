eaee8f0178f48d423602ea18f421c52a0dfb184f
theuzbekN1 <dunyoahli85@gmail.com>
2024-04-19 22:18:49 +0500

    Java faylimizga kodlarni yozdik va tayyor

--- Oylagan sonni top/src/Main.java
+++ Oylagan sonni top/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner nom = new Scanner(System.in);
+        Random nom2 = new Random();
+        int javob, son;
+        System.out.print("Men bir son oyladim osha sonni top: ");
+        javob = nom.nextInt();
+        son = nom2.nextInt(99);
+        while (true){
+            if(javob < son){
+                System.out.print("Men oylagan son kattaroq: ");
+                javob = nom.nextInt();
+            }
+            else if(javob > son){
+                System.out.print("Men oylagan son kichikroq: ");
+                javob = nom.nextInt();
+            }
+            else {
+                System.out.println("Topdingiz");
+                break;
+            }
+        }
     }
 }
